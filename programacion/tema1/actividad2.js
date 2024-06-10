cc78923a7032882891d58f9e63c0a7853ffe5c9b
Pandor17 <beatrizpastorbaena@gmail.com>
2024-06-10 16:39:07 +0200

    ejercicio 1 de programación

---
+++ programacion/tema1/actividad2.js
@@ -0,0 +1,12 @@
+/*Indica y justifica qué tipo de datos de los que hemos estudiado utilizarías para representar lo siguiente:
+1. El precio de una barra de pan.
+2. El nombre de una constelación.
+3. El DNI de una persona.
+4. La cantidad ovejas que hay en un rebaño.
+5. La matrícula de un coche.
+6. La distancia recorrida por un avión durante un vuelo.
+7. La velocidad media de una motocicleta.
+8. La cantidad de calorías que tiene un yogurt.
+9. La fecha de nacimiento de una persona.
+10. El signo del zodíaco de una persona.*/
+
