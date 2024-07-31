c57e3d5fa4445743e61c3bae9d49d43d615704c7
Pandor17 <beatrizpastorbaena@gmail.com>
2024-07-31 12:25:15 +0200

    web y proyecto java

---
+++ programacion/ActividadesJava4_20/src/main/java/com/mycompany/actividadesjava4_20/ActividadesJava4_20.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadesjava4_20;
+
+/**
+ *
+ * @author beapa
+ */
+public class ActividadesJava4_20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ programacion/Tema3/Tema3Act1/src/main/java/com/mycompany/tema3act1/PruebaVariosMain.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tema3act1;
+
+/**
+ *
+ * @author beapa
+ */
+public class PruebaVariosMain {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    } 
+}
